1701f675d7836eb1a9c7c39e61850e14f4da9b3a
Santosh Narayanan <32931478+santoshnarayanan@users.noreply.github.com>
2019-11-08 15:27:34 +0530

    SportsStore
    SportsStore-Version1

---
+++ sportsstore/src/data/Types.js
@@ -0,0 +1,9 @@
+export const DataTypes = {
+    PRODUCTS: "products",
+    CATEGORIES: "categories"
+}
+
+// single action performed called data_load
+export const ActionTypes = {
+    DATA_LOAD: "data_load"
+}
